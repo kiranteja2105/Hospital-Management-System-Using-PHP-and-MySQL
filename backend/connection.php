27e70f8d418e4680ae95527d9a329f9c94468892
kiranteja2105 <kiranteja2130@gmail.com>
2025-06-30 21:49:18 +0530

    commit

--- backend/connection.php
+++ backend/connection.php
@@ -3,7 +3,7 @@
     $username = "root"; 
     $password = "";     
     $dbname = "SSS";
-    $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn = new mysqli($servername, $username, $password, $dbname,3307);
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
